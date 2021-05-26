3fa1d562101f9036a9277026f63915992834838b
sbsatter <shahedbin.satter@un.org>
2021-05-26 23:12:22 +0600

    track start and end times of whole trip

--- src/main/java/com/sbsatter/model/Trip.java
+++ src/main/java/com/sbsatter/model/Trip.java
@@ -1,9 +1,12 @@
 package com.sbsatter.model;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public class Trip {
     private List<Flight> flights;
+    private LocalDateTime begins;
+    private LocalDateTime ends;
     private double cost;
 
     public List<Flight> getFlights() {
@@ -12,6 +15,22 @@ public class Trip {
 
     public void setFlights(List<Flight> flights) {
         this.flights = flights;
+    }
+
+    public LocalDateTime getBegins() {
+        return begins;
+    }
+
+    public void setBegins(LocalDateTime begins) {
+        this.begins = begins;
+    }
+
+    public LocalDateTime getEnds() {
+        return ends;
+    }
+
+    public void setEnds(LocalDateTime ends) {
+        this.ends = ends;
     }
 
     public double getCost() {
@@ -24,6 +43,6 @@ public class Trip {
 
     @Override
     public String toString() {
-        return String.format("Trip[cost: %.2f, flights=%s]", cost, flights);
+        return String.format("Trip[cost: %.2f, starts=%s, ends=%s]\nflights=%s", cost, begins, ends, flights);
     }
 }
